822b66d9975d836c1a9a004d55619d783acae104
jelandriel <aldodiku2017@gmail.com>
2020-03-27 16:54:42 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello world"
+        $x = 10
+        for($i = 0; $i< $x;$i++){
+            ech $i . "<br>"
+
+        }
+    ?>
+</body>
+</html>
